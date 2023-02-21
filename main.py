0a18846a1f614fbd8dbba5100de07bcba8613beb
remi-vidal-michel <remi.vidal-michel@laplateforme.io>
2023-02-21 21:50:23 +0100

    [ADD] added enemies

--- main.py
+++ main.py
@@ -7,25 +7,37 @@ screen = pygame.display.set_mode((900, 500))
 bg = pygame.image.load("assets/wallpaper.png")
 bg_alt = pygame.image.load("assets/wallpaper_alt.png")
 heart = pygame.transform.scale(pygame.image.load("assets/heart.png"), (40, 40))
+small_heart = pygame.transform.scale(heart, (20, 20))
 
 spritesheet_img = pygame.image.load("assets/necromancer.png").convert_alpha()
 sprite_sheet = spritesheet.SpriteSheet(spritesheet_img)
+ghost_img = pygame.image.load("assets/ghost-idle.png").convert_alpha()
+ghost_sheet = spritesheet.SpriteSheet(ghost_img)
 
 font = pygame.font.Font("assets/MatchupPro.otf", 50)
 big_font = pygame.font.Font("assets/MatchupPro.otf", 80)
 small_font = pygame.font.Font("assets/MatchupPro.otf", 40)
 white = (255, 255, 255)
 black = (0, 0, 0)
-green = (0, 128, 0)
+blue = (21, 21, 47)
 
-life = 3
+life = 5
 meter = 0
 lines = open("words.txt").read().splitlines()
 next_word = random.choice(lines)
+monster_cd = 0
+monster_life = 100
 
 # def options():
     
 # def score():
+def monster():
+    global monster_life, monster_cd, monster_timer
+    monster_life = random.randint(3, 5)
+    monster_timer = monster_life
+    print("vie : ", monster_timer)
+    monster_cd = 0
+
 
 def new_word():
     global next_word, pressed_word, lines, text, text_rect, count, count_rect, active_word
@@ -65,18 +77,21 @@ def main_menu():
                 elif quit_rect.collidepoint(mouse_pos):
                     pygame.quit()
     new_word()
+    monster()
 
 main_menu()
 
 # animations
 necro_idle_list = []
-necro_idle_frames = 8
+ghost_idle_list = []
 update = pygame.time.get_ticks()
 animation_cd = 250
 frames = 0
 
-for frame in range (necro_idle_frames):
+for frame in range (0, 8):
     necro_idle_list.append(sprite_sheet.get_image(frame, 160, 128, 3, black))
+for frame in range (0, 7):
+    ghost_idle_list.append(ghost_sheet.get_image(frame, 64, 80, 3, black))
 
 clock = pygame.time.Clock()
 running = True
@@ -95,9 +110,9 @@ while running:
                 if active_word == pressed_word:
                     meter += len(active_word)
                     new_word()
+                    monster_life -= 1
             else:
                 pressed_word = ""
-                life -= 1
     
     screen.blit(bg_alt, (0, 0))
 
@@ -105,14 +120,26 @@ while running:
 
     if current_time - update >= animation_cd:
         frames += 1
+        monster_cd += 1
         update = current_time
-        if frames >= len(necro_idle_list):
+        print(monster_cd, monster_life)
+        if frames >= 7:
             frames = 0
+        if monster_cd == monster_timer * 5:
+            life -= 1
+            monster()
+        if monster_life == 0:
+            print("KILL")
+            monster()
 
     screen.blit(necro_idle_list[frames], (-150, 100))
+    screen.blit(ghost_idle_list[frames], (650, 230))
 
     for hearts in range(life):
         screen.blit(heart, (20 + hearts * 50, 20))
+
+    for hearts in range(monster_life):
+        screen.blit(small_heart, (screen.get_width() // 1.19 - monster_timer * 15 + hearts * 25, 260))
     
     next_surface = small_font.render(next_word, True, white)
     next_rect = next_surface.get_rect()
@@ -120,12 +147,12 @@ while running:
     screen.blit(next_surface, next_rect)
     for i, letter in enumerate(active_word):
         if i < len(pressed_word):
-            color = green
+            color = blue
         else:
             color = white
-        letter_surface = small_font.render(letter, True, color)
+        letter_surface = big_font.render(letter, True, color)
         letter_rect = letter_surface.get_rect()
-        letter_rect.center = (screen.get_width() // 2 - len(active_word) * 7.5 + i * 17, screen.get_height() // 2)
+        letter_rect.center = (screen.get_width() // 2 - len(active_word) * 15 + i * 33, screen.get_height() // 2)
         screen.blit(letter_surface, letter_rect)
     if life == 0:
         main_menu()
